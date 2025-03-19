dec48dbf371ae36aaf5874437f3494aeb3dd518f
Yenleke <2351050210yen@ou.edu.vn>
2025-03-19 11:18:07 +0700

    Add files via upload

---
+++ Sub_FlashCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VietnameseEnglishDictionary
+{
+    public partial class Sub_FlashCard: Form
+    {
+        public Sub_FlashCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
